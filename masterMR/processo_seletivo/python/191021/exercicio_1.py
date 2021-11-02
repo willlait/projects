a0de5b6a89ab4f95962db77e0772da6e8cf5a73e
willlait <62961074+willlait@users.noreply.github.com>
2021-11-02 17:45:54 -0300

    Update exercicio_1.py

--- masterMR/processo_seletivo/python/191021/exercicio_1.py
+++ masterMR/processo_seletivo/python/191021/exercicio_1.py
@@ -1,20 +1,30 @@
-# Desafio: 
+# Desafio:
 # Calculadora sem usar if-else, com as 4 operações básicas.
 
-print('\nBem vindo a Calculadora sem expressões if-else!\nUtilize os simbolos aritméticos para escolher o tipo de operação mais a frente.\n')
+print('\nBem vindo a Calculadora sem expressões if-else!\nUtilize os simbolos aritméticos para escolher o tipo de '
+      'operação mais a frente.\n')
 print('+ = adição\n- = subtração\n* = multiplicação\n/ = divisão\n')
-num1 = float(input('Insira o primeiro número: '))
-num2 = float(input('Insira o segundo número: '))
+
+num1 = input('Insira o primeiro número: ')
+while not num1.isnumeric():
+    num1 = input('Insira um NÚMERO real para o primeiro valor: ')
+
+num2 = input('Insira o segundo número: ')
+while not num2.isnumeric():
+    num2 = input('Insira um NÚMERO real para o segundo valor: ')
+
 operadores = input('Escolha uma operação: ')
+while not operadores in ('+', '-', '*', '/'):
+    operadores = input('Insira um caractere de operação válido: ')
 
-match operadores: 
-        case '+':
-            print(f'Resultado: {num1 + num2}')
-        case '-':
-            print(f'Resultado: {num1 - num2}')
-        case '*':
-            print(f'Resultado: {num1 * num2}')
-        case '/':
-            print(f'Resultado: {num1 / num2}')
+match operadores:
+    case '+':
+        print(f'Resultado: {float(num1) + float(num2)}')
+    case '-':
+        print(f'Resultado: {float(num1) - float(num2)}')
+    case '*':
+        print(f'Resultado: {float(num1) * float(num2)}')
+    case '/':
+        print(f'Resultado: {float(num1) / float(num2)}')
 
-input('') # Mantém o terminal aberto ao final da execução.
+input('')  # Mantém o terminal aberto ao final da execução.
