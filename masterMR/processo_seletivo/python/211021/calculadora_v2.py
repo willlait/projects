6abe87789ad300b536e60c210f80e29f0eb49d6c
willlait <62961074+willlait@users.noreply.github.com>
2021-11-02 17:49:46 -0300

    Update calculadora_v2.py

--- masterMR/processo_seletivo/python/211021/calculadora_v2.py
+++ masterMR/processo_seletivo/python/211021/calculadora_v2.py
@@ -3,20 +3,29 @@
 
 from operator import *
 
-print('\nBem vindo a Calculadora sem expressões if-else & pattern matching!\nUtilize os simbolos aritméticos para escolher o tipo de operação mais a frente.\n')
+print('\nBem vindo a Calculadora sem expressões if-else & pattern matching!\nUtilize os simbolos aritméticos para '
+      'escolher o tipo de operação mais a frente.\n')
 print('+ = adição\n- = subtração\n* = multiplicação\n/ = divisão\n')
-num1 = float(input('Insira o primeiro número: '))
-num2 = float(input('Insira o segundo número: '))
-operador = input('Insira o operador: ')
+num1 = input('Insira o primeiro número: ')
+while not num1.isnumeric():
+    num1 = input('Insira um NÚMERO real para o primeiro valor: ')
+
+num2 = input('Insira o segundo número: ')
+while not num2.isnumeric():
+    num2 = input('Insira um NÚMERO real para o segundo valor: ')
+
+operador = input('Escolha uma operação: ')
+while not operador in ('+', '-', '*', '/'):
+    operador = input('Insira um caractere de operação válido: ')
 
 operadores = {
-    "+" : add(num1, num2),
-    "-" : sub(num1, num2),
-    "/" : truediv(num1, num2),
-    "*" : mul(num1, num2)
+    "+": add(float(num1), float(num2)),
+    "-": sub(float(num1), float(num2)),
+    "/": truediv(float(num1), float(num2)),
+    "*": mul(float(num1), float(num2))
 }
 
 calc = operadores[operador]
 print(calc)
 
-input('') # Mantém o terminal aberto ao final da execução.
+input('')  # Mantém o terminal aberto ao final da execução.
